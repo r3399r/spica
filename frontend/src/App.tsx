4d607b9659e7236eb03975b94484596503e82b8d
Yue <yue@potentia.asia>
2022-10-12 17:21:43 +0800

    layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,9 @@
 import Routes from './Routes';
 
-const App = () => <Routes />;
+const App = () => (
+  <div className="mx-auto px-[15px] max-w-[640px] box-content">
+    <Routes />
+  </div>
+);
 
 export default App;
