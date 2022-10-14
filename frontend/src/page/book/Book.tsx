9b1b405daea60b937b525956d73acf8beebed1f0
Yue <yue@potentia.asia>
2022-10-14 09:06:58 +0800

    improve rwd

--- frontend/src/page/book/Book.tsx
+++ frontend/src/page/book/Book.tsx
@@ -21,26 +21,28 @@ const Book = () => {
         <div>
           <img src={PicBookHero} className="w-full" />
         </div>
-        <H2 className="mt-[30px] mb-5 mx-10">帳簿清單</H2>
-        <div className="flex gap-[10px] mx-10 flex-wrap">
-          {bookList?.map((v, i) => (
-            <div
-              key={v.id}
-              className={classNames(
-                'h-[100px] w-[calc(50%-5px)] rounded-[15px] px-[10px] pt-[10px]',
-                {
-                  ['bg-beige-300']: i % 3 === 0,
-                  ['bg-green-300']: i % 3 === 1,
-                  ['bg-tan-300']: i % 3 === 2,
-                },
-              )}
-            >
-              <div className="bg-white w-fit h-fit rounded-full mb-[10px]">
-                <img src={IcBook} />
+        <div className="max-w-[640px] mx-[15px] sm:mx-auto">
+          <H2 className="mt-[30px] mb-5 mx-[25px]">帳簿清單</H2>
+          <div className="flex gap-[10px] mx-[25px] flex-wrap">
+            {bookList?.map((v, i) => (
+              <div
+                key={v.id}
+                className={classNames(
+                  'min-h-[100px] w-[calc(50%-5px)] rounded-[15px] px-[10px] pt-[10px] pb-2 sm:w-[140px]',
+                  {
+                    ['bg-beige-300']: i % 3 === 0,
+                    ['bg-green-300']: i % 3 === 1,
+                    ['bg-tan-300']: i % 3 === 2,
+                  },
+                )}
+              >
+                <div className="bg-white w-fit h-fit rounded-full mb-[10px]">
+                  <img src={IcBook} />
+                </div>
+                <div className="text-[14px] leading-normal break-words">{v.name}</div>
               </div>
-              <div className="text-[14px] leading-normal break-words">{v.name}</div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
       <div className="fixed bottom-0 h-[104px] w-full">
