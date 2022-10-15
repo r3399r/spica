ff9fc8e3c9cb3b02da40647f41f4377771ade0d2
Yue Change <r3399r@gmail.com>
2022-10-15 23:28:46 +0800

    add Input component

---
+++ frontend/src/component/Input.tsx
@@ -0,0 +1,43 @@
+import classNames from 'classnames';
+import { InputHTMLAttributes } from 'react';
+
+type Props = InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+  helper?: string;
+  error?: string;
+};
+
+const Input = ({ label, helper, error, disabled, ...props }: Props) => (
+  <div className="py-4">
+    {label && (
+      <div
+        className={classNames('text-[14px] leading-normal text-navy-700 mb-[5px]', {
+          'opacity-30': disabled,
+        })}
+      >
+        {label}
+      </div>
+    )}
+    <input
+      className={classNames(
+        'rounded bg-grey-200 outline-none p-2 h-[40px] border-solid border-[1px] focus:border-solid focus:border-teal-500',
+        {
+          'border-tomato-500': !!error,
+          'border-grey-200': !error,
+          'text-grey-500 placeholder:text-grey-500': !!disabled,
+          'text-navy-900 placeholder:text-navy-100': !disabled,
+        },
+      )}
+      disabled={disabled}
+      {...props}
+    />
+    {(error || helper) && (
+      <div className="mt-[5px]">
+        {error && <div className="text-tomato-500 text-[12px] leading-normal">{error}</div>}
+        {helper && <div className="text-navy-300 text-[12px] leading-normal">{helper}</div>}
+      </div>
+    )}
+  </div>
+);
+
+export default Input;
