a9c2c376ee345d051f6ca6823207eb733ee4ee09
Yue <yue@potentia.asia>
2022-10-25 16:56:20 +0800

    edit nickname feature

--- frontend/src/page/member/MemberList.tsx
+++ frontend/src/page/member/MemberList.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from 'react';
+import { Member } from '@y-celestial/spica-service/lib/src/model/entity/Member';
+import { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
@@ -7,28 +8,40 @@ import IcEdit from 'src/image/ic-edit.svg';
 import IcRemoveDisabled from 'src/image/ic-remove-disabled.svg';
 import IcRemove from 'src/image/ic-remove.svg';
 import { RootState } from 'src/redux/store';
+import ModalEditMember from './ModalEditMember';
 
 const MemberList = () => {
   const { id } = useParams();
   const { t } = useTranslation();
   const { books } = useSelector((rootState: RootState) => rootState.book);
   const members = useMemo(() => books?.find((v) => v.id === id)?.members ?? null, [books]);
+  const [target, setTarget] = useState<Member>();
 
   if (members === null || members.length === 0)
     return <Body className="text-navy-300 py-[30px] text-center">{t('member.noMemberHint')}</Body>;
 
   return (
-    <div className="px-[10px] mb-5">
-      {members.map((v) => (
-        <div key={v.id} className="py-[10px] flex justify-between border-b-[1px] border-b-grey-100">
-          <div>{v.nickname}</div>
-          <div className="flex gap-[15px]">
-            <img src={IcEdit} />
-            {v.deletable === true ? <img src={IcRemove} /> : <img src={IcRemoveDisabled} />}
+    <>
+      <div className="px-[10px] mb-5">
+        {members.map((v) => (
+          <div
+            key={v.id}
+            className="py-[10px] flex justify-between border-b-[1px] border-b-grey-100"
+          >
+            <div>{v.nickname}</div>
+            <div className="flex gap-[15px]">
+              <img src={IcEdit} className="cursor-pointer" onClick={() => setTarget(v)} />
+              {v.deletable === true ? (
+                <img src={IcRemove} className="cursor-pointer" />
+              ) : (
+                <img src={IcRemoveDisabled} className="cursor-pointer" />
+              )}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <ModalEditMember open={!!target} handleClose={() => setTarget(undefined)} target={target} />
+    </>
   );
 };
 
---
+++ frontend/src/page/member/ModalEditMember.tsx
@@ -0,0 +1,49 @@
+import { Member } from '@y-celestial/spica-service/lib/src/model/entity/Member';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import FormInput from 'src/component/celestial-ui/FormInput';
+import ModalForm from 'src/component/celestial-ui/ModalForm';
+import { RenameMemberForm } from 'src/model/Form';
+import { renameMember } from 'src/service/memberService';
+
+type Props = {
+  open: boolean;
+  handleClose: () => void;
+  target?: Member;
+};
+
+const ModalEditMember = ({ open, handleClose, target }: Props) => {
+  const { t } = useTranslation();
+  const methods = useForm<RenameMemberForm>();
+
+  useEffect(() => {
+    methods.setValue('nickname', target?.nickname ?? '');
+  }, [target]);
+
+  const onClose = () => {
+    handleClose();
+    methods.reset();
+  };
+
+  const onSubmit = (data: RenameMemberForm) => {
+    if (!target) return;
+    renameMember(target.bookId, target.id, data.nickname).then(onClose);
+  };
+
+  return (
+    <ModalForm
+      methods={methods}
+      onSubmit={onSubmit}
+      open={open}
+      handleClose={onClose}
+      title={t('member.edit')}
+      cancelBtn={t('act.cancel')}
+      confirmBtn={t('act.submit')}
+    >
+      <FormInput name="nickname" label={t('member.nickname')} autoFocus required />
+    </ModalForm>
+  );
+};
+
+export default ModalEditMember;
