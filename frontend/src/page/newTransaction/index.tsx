066c2672d53fa5d48e180d9d4773b4de4de6d321
Yue <yue@potentia.asia>
2022-11-15 23:42:54 +0800

    add sharer and payer

--- frontend/src/page/newTransaction/index.tsx
+++ frontend/src/page/newTransaction/index.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
 import Button from 'src/component/celestial-ui/Button';
 import DatetimePicker from 'src/component/celestial-ui/DatetimePicker';
 import Divider from 'src/component/celestial-ui/Divider';
@@ -11,17 +13,33 @@ import FormTextarea from 'src/component/celestial-ui/FormTextarea';
 import Select from 'src/component/celestial-ui/Select';
 import SelectOption from 'src/component/celestial-ui/SelectOption';
 import Body from 'src/component/celestial-ui/typography/Body';
+import { Page } from 'src/constant/Page';
+import IcEdit from 'src/image/ic-edit-tx.svg';
 import { BillForm } from 'src/model/Form';
+import { RootState } from 'src/redux/store';
+import { loadBookById } from 'src/service/bookService';
+import { randomPick } from 'src/util/random';
 import Navbar from './Navbar';
 
 const NewTransaction = () => {
+  const { id } = useParams();
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const methods = useForm<BillForm>();
+  const { books } = useSelector((rootState: RootState) => rootState.book);
+  const members = useMemo(() => books?.find((v) => v.id === id)?.members, [books]);
   const [value, setValue] = useState<Date>(new Date());
+
+  useEffect(() => {
+    if (id === undefined) return;
+    loadBookById(id).catch(() => navigate(Page.Book));
+  }, [id]);
 
   const onSubmit = (data: BillForm) => {
     console.log(data);
   };
+
+  if (!members) return <></>;
 
   return (
     <>
@@ -31,7 +49,7 @@ const NewTransaction = () => {
           <Form onSubmit={onSubmit} methods={methods}>
             <div className="flex gap-4 pb-4">
               <div className="w-[108px]">
-                <Select label={t('newTx.type')}>
+                <Select label={t('newTx.type')} defaultValue="out">
                   <SelectOption value="out">{t('desc.out')}</SelectOption>
                   <SelectOption value="in">{t('desc.in')}</SelectOption>
                   <SelectOption value="transfer">{t('desc.transfer')}</SelectOption>
@@ -54,10 +72,29 @@ const NewTransaction = () => {
             <div className="pb-4">
               <FormNumberInput decimal={2} name="amount" label={t('newTx.amount')} />
             </div>
-            <Body>{t('desc.payer')}</Body>
-            <Divider />
-            <Body>{t('desc.sharer')}</Body>
-            <Divider />
+            <Body className="mb-[5px] text-navy-700">{t('desc.payer')}</Body>
+            <div className="flex justify-between gap-[10px]">
+              <div className="ml-[10px] flex justify-between flex-1">
+                <Body size="l">{randomPick(members).nickname}</Body>
+                <Body size="l" className="text-navy-300">
+                  {t('newTx.all')}
+                </Body>
+              </div>
+              <div>
+                <img src={IcEdit} className="cursor-pointer" />
+              </div>
+            </div>
+            <Divider className="my-[15px]" />
+            <Body className="mb-[5px] text-navy-700">{t('desc.sharer')}</Body>
+            <div className="flex justify-between gap-[10px]">
+              <div className="ml-[10px] flex justify-between flex-1">
+                <Body size="l">{t('newTx.allShare')}</Body>
+              </div>
+              <div>
+                <img src={IcEdit} className="cursor-pointer" />
+              </div>
+            </div>
+            <Divider className="my-[15px]" />
             <FormTextarea name="memo" label={t('desc.memo')} />
             <button className="hidden" />
           </Form>
