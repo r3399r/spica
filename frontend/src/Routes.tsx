7a0b172fb3deac9282c5ae73e5bb0d96b04bb4b7
Yue <yue@potentia.asia>
2022-12-13 17:14:53 +0800

    test adsense

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -4,6 +4,7 @@ import BookDetail from './page/bookDetail';
 import BookList from './page/bookList';
 import BookSetting from './page/bookSetting';
 import EidtTransaction from './page/editTransaction';
+import Landing from './page/landing/Landing';
 import Member from './page/member';
 import PersonalBalance from './page/personalBalance';
 import Settlement from './page/settlement';
@@ -12,7 +13,7 @@ import Transaction from './page/transaction';
 
 const AppRoutes = () => (
   <Routes>
-    {/* <Route path={Page.Landing} element={<Landing />} /> */}
+    <Route path={Page.Landing} element={<Landing />} />
     <Route path={Page.Book} element={<BookList />} />
     <Route path={`${Page.Book}/:id`} element={<BookDetail />} />
     <Route path={`${Page.Book}/:id/tx`} element={<EidtTransaction />} />
--- frontend/src/page/landing/Landing.tsx
+++ frontend/src/page/landing/Landing.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from 'src/celestial-ui/component/Button';
 import H1 from 'src/celestial-ui/component/typography/H1';
@@ -11,6 +12,11 @@ import { Page } from 'src/constant/Page';
 const Landing = () => {
   const navigate = useNavigate();
 
+  useEffect(() => {
+    if (typeof window === 'object')
+      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
+  }, []);
+
   return (
     <div>
       <H1>h1 Heading 1 標題 1</H1>
@@ -21,6 +27,17 @@ const Landing = () => {
       <H6>h6 heading 6 標題 6</H6>
       <p>Alle Menschen sind frei und gleich an Würde und Rechten</p>
       <Button onClick={() => navigate(Page.Book)}>START</Button>
+      <div className="ad">
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-format="fluid"
+          data-ad-layout-key="-fb+5w+4e-db+86"
+          data-ad-client="ca-pub-4116238563422892"
+          data-adtest="on"
+          data-ad-slot="2305807594"
+        />
+      </div>
     </div>
   );
 };
