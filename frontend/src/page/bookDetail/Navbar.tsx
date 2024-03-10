9b231c9412266b7446248cae5c0d202f145bbbab
Yue Chang <lamplighter.planet@gmail.com>
2024-03-10 12:11:50 +0800

    add share icon

--- frontend/src/page/bookDetail/Navbar.tsx
+++ frontend/src/page/bookDetail/Navbar.tsx
@@ -1,26 +1,47 @@
 import { useState } from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import BackButton from 'src/component/BackButton';
 import Img from 'src/component/Img';
 import ModalExport from 'src/component/ModalExport';
 import { Page } from 'src/constant/Page';
+import useBook from 'src/hook/useBook';
 import IcExportActive from 'src/image/ic-export-active.svg';
 import IcExport from 'src/image/ic-export.svg';
 import IcSettingActive from 'src/image/ic-setting-active.svg';
 import IcSetting from 'src/image/ic-setting.svg';
+import IcShare from 'src/image/ic-share.svg';
+import { setSnackbarMessage } from 'src/redux/uiSlice';
 import { exportOverallPdf } from 'src/service/bookService';
 
 const Navbar = () => {
   const { id } = useParams();
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [open, setOpen] = useState<boolean>(false);
+  const book = useBook();
+  const link = `${location.origin}/book/${id}?a=1`;
+
+  const onShareLink = () => {
+    if (!navigator.share || book === undefined) return;
+    navigator.share({
+      text: t('member.shareText', { book: book.name }),
+      url: link,
+    });
+  };
 
   return (
     <div className="mb-5 mt-[15px] flex justify-between">
       <BackButton text={t('bookDetail.back')} />
       <div className="flex gap-[15px]">
+        <CopyToClipboard text={link} onCopy={() => dispatch(setSnackbarMessage(t('desc.copy')))}>
+          <div className="cursor-pointer" onClick={onShareLink}>
+            <Img src={IcShare} />
+          </div>
+        </CopyToClipboard>
         <div className="cursor-pointer" onClick={() => setOpen(true)}>
           <Img src={IcExport} srcActive={IcExportActive} />
         </div>
