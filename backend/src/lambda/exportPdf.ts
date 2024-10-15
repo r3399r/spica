2351a2edd8c33d938a54be8f4e9e6c93267f1eec
yue.chang <lamplighter.planet@gmail.com>
2024-10-16 04:27:53 +0800

    add export pdf monitor

---
+++ backend/src/lambda/exportPdf.ts
@@ -0,0 +1,45 @@
+import { BadRequestError, InternalServerError } from 'src/model/error';
+import { LambdaContext, LambdaEvent } from 'src/model/Lambda';
+
+let event: LambdaEvent;
+
+export async function exportPdf(
+  lambdaEvent: LambdaEvent,
+  _context?: LambdaContext
+) {
+  event = lambdaEvent;
+
+  switch (event.resource) {
+    case '/api/exportPdf/{id}':
+      return await apiExportPdfId();
+    case '/api/exportPdf/{id}/member/{mid}':
+      return await apiExportPdfIdMemberMid();
+  }
+  throw new InternalServerError('unknown resource');
+}
+
+async function apiExportPdfId() {
+  if (event.pathParameters === null)
+    throw new BadRequestError('pathParameters should not be empty');
+  if (event.headers === null)
+    throw new BadRequestError('headers should not be empty');
+  switch (event.httpMethod) {
+    case 'PATCH':
+      return
+    default:
+      throw new InternalServerError('unknown http method');
+  }
+}
+
+async function apiExportPdfIdMemberMid() {
+  if (event.pathParameters === null)
+    throw new BadRequestError('pathParameters should not be empty');
+  if (event.headers === null)
+    throw new BadRequestError('headers should not be empty');
+  switch (event.httpMethod) {
+    case 'PATCH':
+      return
+    default:
+      throw new InternalServerError('unknown http method');
+  }
+}
