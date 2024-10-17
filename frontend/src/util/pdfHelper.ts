6a927758c0a73dec0154277e9326f06a1fc1c3ca
yue.chang <lamplighter.planet@gmail.com>
2024-10-17 23:27:18 +0800

    improve pdf helper

--- frontend/src/util/pdfHelper.ts
+++ frontend/src/util/pdfHelper.ts
@@ -1,34 +1,17 @@
-import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
+import html2pdf from 'html2pdf.js';
 
-export const exportPdf = async (elementId: string, filename: string, margin = 10) => {
+export const exportPdf = async (elementId: string, filename: string) => {
   const input = document.getElementById(elementId);
   if (input === null) return;
 
-  const canvas = await html2canvas(input);
-  const imgData = canvas.toDataURL('image/png');
-
-  const pageHeight = 297;
-  const imgWidth = 210 - 2 * margin;
-  const imgHeight = (imgWidth * canvas.height) / canvas.width;
-
-  let heightLeft = imgHeight;
-  let paddingTop = 0;
-
-  const doc = new jsPDF({
-    unit: 'px',
-    format: [297, 210],
-  });
-
-  doc.addImage(imgData, 'PNG', margin, paddingTop, imgWidth, imgHeight, 'alias', 'FAST');
-  heightLeft -= pageHeight;
-
-  while (heightLeft >= 0) {
-    paddingTop = heightLeft - imgHeight; // top padding for other pages
-    doc.addPage();
-    doc.addImage(imgData, 'PNG', margin, paddingTop, imgWidth, imgHeight, 'alias', 'FAST');
-    heightLeft -= pageHeight;
-  }
-
-  doc.save(filename);
+  html2pdf()
+    .set({
+      filename,
+      image: {
+        type: 'jpeg',
+        quality: 1,
+      },
+    })
+    .from(input)
+    .save();
 };
