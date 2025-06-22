e46954ae0a6bd7aa05e9496eb056077b8ba2de3e
yue.chang <lamplighter.planet@gmail.com>
2025-06-22 15:38:23 +0800

    simple test

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Loader from './component/Loader'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,12 +7,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        {/* <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
+        {/* <a href="h */}
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -28,6 +24,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Loader/>
     </>
   )
 }
---
+++ webapp/src/component/Loader.tsx
@@ -0,0 +1,18 @@
+import { Backdrop } from '@mui/material';
+// import { useSelector } from 'react-redux';
+import IcLoader from 'src/image/ic-loader.svg';
+// import { RootState } from 'src/redux/store';
+
+const Loader = () => {
+  // const { workload } = useSelector((rootState: RootState) => rootState.ui);
+
+  return (
+    <Backdrop open={true} className="z-[1400]">
+      <div className="w-[80px] outline-none">
+        <img src={IcLoader} />
+      </div>
+    </Backdrop>
+  );
+};
+
+export default Loader;
